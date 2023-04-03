6185730c8b22db9c4d6b2c19474608cad812aa50
Ziran10 <victor.antunes01@protonmail.com>
2023-04-03 15:13:42 -0300

    atividades

--- Atividade 2/script2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-num_processos=$(ps aux | wc -l)
-
-echo "Existem $num_processos processos em execução"
-
--- Atividade 2/script4.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ $# -eq 0 ]; then
-  echo "Nenhum parâmetro foi passado."
-else
-  echo "Foram passados $# parâmetros."
-fi
